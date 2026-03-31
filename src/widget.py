3f0cd6adbdbb515a9ca477f95a0111ba3b9eb11e
YourName <tutu@example.com>
2026-03-31 17:55:08 +0500

    Add widget modul

--- src/widget.py
+++ src/widget.py
@@ -1,4 +1,24 @@
 from typing import Union
+from src.masks import get_mask_account,get_mask_card_number
 
+def mask_account_card (info: str) ->str:
+    """Принимаем строку с типом и номером/счетом карты
+    и возвращаем замаскированную версию"""
+    #Разделяем строку по пробелам
+    parts = info.split()
+    #Последний элемент в списке НОМЕР
+    number_str = parts[-1]
+    #Элементы до последнего это название, склеиваем их обратно
+    type_name = ''.join(parts[:-1])
+    #Проверяем счет или карта
+    if type_name.lower() == 'счет':
+        # Если это счет, вызываем функцию для счета
+        masked_number = get_mask_account(int(number_str))
+    else:
+        #Если это карта, вызываем функию для карты
+        masked_number = get_mask_card_number(int(number_str))
 
-def mask_account_card (name:Union[str, int]):
+        #Возвращаем готовую маскировку
+        return f'{type_name} {masked_number}'
+
+def get_date(data)
