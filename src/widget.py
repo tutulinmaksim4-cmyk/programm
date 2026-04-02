f96228efe2fa229dc958ae10e81c1695ac3ba509
YourName <tutu@example.com>
2026-04-02 16:28:12 +0500

    correction mypy

--- src/widget.py
+++ src/widget.py
@@ -1,30 +1,31 @@
-from typing import Union
-from src.masks import get_mask_account,get_mask_card_number
+from datetime import datetime
 
-def mask_account_card (info: str) ->str:
+from src.masks import get_mask_account, get_mask_card_number
+
+
+def mask_account_card(info: str) -> str:
     """Принимаем строку с типом и номером/счетом карты
     и возвращаем замаскированную версию"""
-    #Разделяем строку по пробелам
+    # Разделяем строку по пробелам
     parts = info.split()
-    #Последний элемент в списке НОМЕР
+    # Последний элемент в списке НОМЕР
     number_str = parts[-1]
-    #Элементы до последнего это название, склеиваем их обратно
-    type_name = ''.join(parts[:-1])
-    #Проверяем счет или карта
-    if type_name.lower() == 'счет':
+    # Элементы до последнего это название, склеиваем их обратно
+    type_name = " ".join(parts[:-1])
+    # Проверяем счет или карта
+    if type_name.lower() == "счет":
         # Если это счет, вызываем функцию для счета
         masked_number = get_mask_account(int(number_str))
     else:
-        #Если это карта, вызываем функию для карты
+        # Если это карта, вызываем функию для карты
         masked_number = get_mask_card_number(int(number_str))
 
-        #Возвращаем готовую маскировку
-        return f'{type_name} {masked_number}'
+        # Возвращаем готовую маскировку
+        return f"{type_name} {masked_number}"
 
-from datetime import datetime
 
-def get_date(data_str: str) -> str:
-    #Принимаем строку с датой в исходном формате
-    dt_object = datetime.strptime(data_str, '%Y-%m-%dT%H:%M^%S.%f')
-    return dt_object.strptime('%d.%m.%Y')
-
+def get_date(date_str: str) -> str:
+    """ Принимаем строку с датой в исходном формате
+     и выводим измененный формат"""
+    dt_object = datetime.strptime(date_str, "%Y-%m-%dT%H:%M^%S.%f")
+    return dt_object.strftime("%d.%m.%Y")
