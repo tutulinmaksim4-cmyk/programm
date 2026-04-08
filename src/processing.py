a639d0b513010f9eb10ef0f7751a6ec85cb2cee7
YourName <tutu@example.com>
2026-04-08 15:47:35 +0500

    changes liters

--- src/processing.py
+++ src/processing.py
@@ -1,13 +1,13 @@
 from typing import Any, Dict, List
 
-def filter_by_state (data: List[Dict[str, Any]], state: str = "EXECUTED") -> List[Dict[str, Any]]:
+
+def filter_by_state(data: List[Dict[str, Any]], state: str = "EXECUTED") -> List[Dict[str, Any]]:
     """Функция фильтрует список словарей по значению ключа state"""
 
     return [item for item in data if item.get("state") == state]
 
 
-
-def sort_by_date (data: List[Dict[str, Any]], reverse: bool = True) -> List[Dict[str, Any]]:
+def sort_by_date(data: List[Dict[str, Any]], reverse: bool = True) -> List[Dict[str, Any]]:
     """Функция сортировки по дате"""
 
-    return sorted(data, key=lambda x:x["data"], reverse=reverse)
+    return sorted(data, key=lambda x: x["data"], reverse=reverse)
