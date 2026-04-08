7baf9c639fe143d7eb9da68099bcbfa69252b6f6
YourName <tutu@example.com>
2026-04-08 15:18:47 +0500

    new function sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -7,4 +7,6 @@ def filter_by_state (data: List[Dict[str, Any]], state: str = "EXECUTED") -> Lis
 
 
 
+def sort_by_date (data: List[Dict[str, Any]], reverse: bool = True) -> List[Dict[str, Any]]:
 
+    return sorted(data, key=lambda x:x["data"], reverse=reverse)
